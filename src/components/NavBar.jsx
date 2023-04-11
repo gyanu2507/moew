51f861c1d4e6e075efac6001ed661b895d90ece8
gyanu2507 <81113356+gyanu2507@users.noreply.github.com>
2023-04-11 12:52:18 +0530

    Added Tailwind

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div >
+      <img src="https://cdn-icons-png.flaticon.com/512/4221/4221419.png"></img>
+      <h3> Movies </h3>
+      <h3> Favourites </h3>
+    </div>
+  );
+}
+
+export default NavBar;
